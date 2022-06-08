b5be55013b5d681673c87bd6340f867476a3476a
Manashacker <ma8809227291@gmail.com>
2022-06-08 14:16:39 +0530

    initial commit

---
+++ static/1.py
@@ -0,0 +1,25 @@
+# from playsound import playsound
+# playsound('C:\\Users\\MINNI\\Documents\\2\\Eminem_-_Rap_God.mp3')
+
+
+
+# print("My name is Manas Kumar")
+# print("My name is Manas KUmar. I am 12 years old.")
+
+# x = input("What is your name ")
+# print("Hey", x)
+
+
+# print("I am a calculator")
+# a = input("Enter the first number of which you want to find the sum of ")
+# a = int(a)
+# b = input("Enter the second number of which you want to find the sum of ")
+# b = int(b)
+# ab = a+b
+# print(f"The sum of {a} and {b} is {ab}")
+x = input("Enter the first number of which you want to find the difference of ")
+x = int(x)
+y = input("Enter the second numbaer of which you want to find the difference of ")
+y = int(y)
+xy = x-y
+print(f"The difference of {x} and {y} is {xy}")
