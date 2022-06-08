9028e4a7d648b8f9b392cb62501612d3b551c4ef
Manashacker <ma8809227291@gmail.com>
2022-06-08 14:47:29 +0530

    commit

---
+++ static/2.py
@@ -0,0 +1,17 @@
+x = input("What is your name ")
+print("Hey", x)
+
+
+print("I am a calculator")
+a = input("Enter the first number of which you want to find the sum of ")
+a = int(a)
+b = input("Enter the second number of which you want to find the sum of ")
+b = int(b)
+ab = a+b
+print(f"The sum of {a} and {b} is {ab}")
+x = input("Enter the first number of which you want to find the difference of ")
+x = int(x)
+y = input("Enter the second numbaer of which you want to find the difference of ")
+y = int(y)
+xy = x-y
+print(f"The difference of {x} and {y} is {xy}")
